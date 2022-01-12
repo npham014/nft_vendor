5f8730234fa3d2616fd39930bcfd52f379b0e656
npham014 <npham014@ucr.edu>
2022-01-12 15:34:53 -0800

    skeleton

---
+++ src/contracts/StickMan.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract StickMan is ERC721 {
+
+    constructor() ERC721("StickMan","STKM") {
+
+    }
+
+    struct Stick {
+        uint id;
+        uint DNA;
+    }
+
+}
---
+++ src/test/StickMan.test.js
@@ -0,0 +1,13 @@
+const StickMan = artifacts.require('./StickMan.sol');
+
+require('chai').use(require('chai-as-promised')).should()
+
+contract('StickMan', (accounts) => {
+
+    describe('deployment', async () => {
+        it('deploys to blockchain', async () => {
+            
+        })
+    })
+
+})
