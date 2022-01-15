065c31aa6a27255d66a262f1acef924519ddc37c
npham014 <npham014@ucr.edu>
2022-01-14 16:10:33 -0800

    First commit

--- src/contracts/StickMan.sol
+++ src/contracts/StickMan.sol
@@ -3,11 +3,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract StickMan is ERC721 {
+    
 
     constructor() ERC721("StickMan","STKM") {
 
     }
 
+    function mint(string memory _dna) public { //gonna let whoever wants one have a stickman, this could be restricted only to a minter address though
+
+    }
     struct Stick {
         uint id;
         uint DNA;
