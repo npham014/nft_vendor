3717edf69ee746527f152ac0330af6de46d0ec79
npham014 <npham014@ucr.edu>
2022-01-17 20:20:43 -0800

    continued work

--- src/contracts/StickMan.sol
+++ src/contracts/StickMan.sol
@@ -1,20 +1,22 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract StickMan is ERC721 {
-    
+contract StickMan is ERC721Enumerable {
+
+    uint[] public StickMans;
+    mapping(uint => bool) _stickExists;
 
     constructor() ERC721("StickMan","STKM") {
-
+        
     }
 
-    function mint(string memory _dna) public { //gonna let whoever wants one have a stickman, this could be restricted only to a minter address though
-
-    }
-    struct Stick {
-        uint id;
-        uint DNA;
+    function mint(uint _dna) public { //gonna let whoever wants one have a stickman, this could be restricted only to a minter address though
+        require(!_stickExists[_dna]);
+        StickMans.push(_dna);
+        uint _id = StickMans.length - 1;
+        _mint(msg.sender, _id);
+        _stickExists[_dna] = true;
     }
 
 }
--- test/StickMan.test.js
+++ test/StickMan.test.js
@@ -29,6 +29,42 @@ contract('StickMan', (accounts) => {
             assert.equal(symbol, "STKM");
         })
 
-    })
+    })// end describe deployment
 
+    describe('minting', async () => {
+        it('mints a new token',async () => {
+            let newToken = await contract.mint(1234);
+            //console.log(contract)
+            let supp = await contract.totalSupply();
+            
+            //should succeed
+            assert.equal(supp, 1);
+            const event = newToken.logs[0].args;
+            assert.equal(event.tokenId.toNumber(), 0, 'id of first token is 0');
+            assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'minted from blank address');
+            assert.equal(event.to,accounts[0], 'to account is correct');
+
+            //should fail
+            await contract.mint("1234").should.be.rejected;
+        }) 
+    })//end describe minting
+
+    describe('nft index', async () => {
+        it('returns all stickmans', async () => {
+            await contract.mint(1111);
+            await contract.mint(2222);
+            await contract.mint(3333);
+            const totSupp = await contract.totalSupply();
+
+            let stick;
+            let actualStickMans = [];
+
+            for (let i = 0; i < totSupp; i++) {
+                stick = await contract.StickMans(i);
+                actualStickMans.push(stick);
+            }
+            let expectedStickMans = [1234,1111,2222,3333];
+            assert.equal(expectedStickMans.join(','),actualStickMans.join(','), "multiple mints can be retrieved");
+        }) 
+    })
 })
